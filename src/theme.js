3e3e69294ed1ae0a3c4153b1c9f470cecc9ef7b3
Harish Babu B K <harishbabubk@Harishs-MacBook-Pro.local>
2022-01-21 17:53:23 +0530

    new status

---
+++ src/theme.js
@@ -0,0 +1,28 @@
+import { createTheme } from '@mui/material';
+
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#6599FF',
+    },
+    // secondary: {
+    //   main: '#ff0',
+    // },
+    // error: {
+    //   main: '#f00',
+    // },
+    // background: {
+    //   default: '#000',
+    // },
+    // textColor: '#000',
+    // primary1Color: '#fff',
+    // primary2Color: '#ff0',
+    // accent1Color: '#f00',
+    // pickerHeaderColor: '#f00',
+    // alternateTextColor: '#f00'
+  },
+});
+
+export default theme;
