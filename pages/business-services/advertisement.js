24860700b48c67daacab3c111dccddc7aa88e61c
Harish Babu B K <harishbabubk@Harishs-MacBook-Pro.local>
2022-01-27 11:30:09 +0530

    Removed Service

--- pages/business-services/advertisement.js
+++ pages/business-services/advertisement.js
@@ -13,7 +13,7 @@ export default function Advertisement() {
 
       <main className={styles.main}>
       <h1 className={styles.title}>Advertisement</h1>
-        <p>Basic Advertisement:-</p>
+        {/* <p>Basic Advertisement:-</p>
         <p>In Basic Advertisement a Model will explain your product while Showroom or Product play in Background.This Ad can be used for Advertising in Youtube, Facebook, Other Social Media and TV.</p>
         <p>Story Advertisement:-</p>
         <p>In Story Advertisement there will be a situation, We will showcase your product as solution for the situation.Example will be Saying in a Detergent advertisement stains are good and For Cleaning stains xyz detergent can help.</p>
@@ -22,7 +22,7 @@ export default function Advertisement() {
         <p>For Example:-</p>
         <p>Let's say we have to advertise a Restaurant.We will create a scene in the restaurant show the dishes delicious and Make actors Talk about the food.
           Let's assume we have to promote a property,We will shoot scenes of the movie in the property and exhibit as great place to be.</p>
-
+ */}
       </main>
 
       <footer className={styles.footer}>
