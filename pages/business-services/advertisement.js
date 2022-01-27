2168625b6e34654e62803a621a3cc189a7de0a58
Harish Babu B K <harishbabubk@Harishs-MacBook-Pro.local>
2022-01-27 12:28:28 +0530

    remove the no

--- pages/business-services/advertisement.js
+++ pages/business-services/advertisement.js
@@ -13,14 +13,14 @@ export default function Advertisement() {
 
       <main className={styles.main}>
       <h1 className={styles.title}>Advertisement</h1>
-       Basic Advertisement 
+       {/* Basic Advertisement 
        In Basic Advertisement a Model will explain your product while Showroom or Product play in Background.This Ad can be used for Advertising in Youtube, Facebook, Other Social Media and TV. 
        Story Advertisement 
        In Story Advertisement there will be a situation, We will showcase your product as solution for the situation.Example will be Saying in a Detergent advertisement stains are good and For Cleaning stains xyz detergent can help. 
        Movie Advertisement 
        We ALIV Cinema Produce Original content such as Short Films, Tele Films and Feature Film for Our Website Release ,Youtube Release,Other OTT Release ,Theatrical and Direct TV Release thus creating great recall Value. 
        For Example
-       Lets say we have to advertise a Restaurant.We will create a scene in the restaurant show the dishes delicious and Make actors Talk about the food. Let's assume we have to promote a property,We will shoot scenes of the movie in the property and exhibit as great place to be.
+       Lets say we have to advertise a Restaurant.We will create a scene in the restaurant show the dishes delicious and Make actors Talk about the food. Let's assume we have to promote a property,We will shoot scenes of the movie in the property and exhibit as great place to be. */}
       </main>
     </div>
   )
